d89485e0c9a16156e1107d6569d8ab006513202a
yaewon94 <84373511+yaewon94@users.noreply.github.com>
2023-12-06 20:26:31 +0900

    Update 05-functions.cpp

--- 05-functions.cpp
+++ 05-functions.cpp
@@ -1,18 +1,32 @@
 // 함수선언
 // 선언, 정의를 동시에 할 수도 있음
-int GetFactorial(int);
+// 함수 호출부분보다 선언부분이 항상 위에 있어야 함 ! (컴파일러가 위에서 아래로 읽으므로)
+// 가독성을 위해 선언부분은 헤더파일로 따로 묶음
+//int GetFactorial(int);
+//int GetFibonacci(int);
+#include "FunctionTest.h"
 
 int main()
 {
-	int result = GetFactorial(3);
+	int result1 = GetFactorial(3);
+	int result2 = GetFibonacci(10);
 
 	return 0;
 }
 
-// 함수정의
-// 재귀함수 : 간단한 문제 해결이 가능한 경우 사용 (ex.계층구조(트리) 순회)
+// 재귀함수 : 간단하게 문제 해결이 가능한 경우 사용
+// 장점 : 코드의 간결성, 직관성
+// 단점 : 느림
+// ex. 팩토리얼
 int GetFactorial(int num)
 {
 	if (num == 1) return num;
-	else num * GetFactorial(--num);
+	else return num * GetFactorial(--num);
+}
+// ex. 피보나치 수열 : num이 클 경우 적합하지 않음 (함수 호출 횟수 : 2^1 +2^2 + ... + 2^num)
+// 1 1 2 3 5 8 13 21 ...
+int GetFibonacci(int num)
+{
+	if (num == 1 || num == 2) return 1;
+	else return GetFibonacci(num-1) + GetFibonacci(num-2);
 }
