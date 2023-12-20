0371fefb2838197bce7753ec450ba87643e3b8bf
yaewon94 <84373511+yaewon94@users.noreply.github.com>
2023-12-20 20:02:28 +0900

    Add files via upload

---
+++ LinkedList.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//
+// C++ list.h 에 구현되어 있음
+//
+
+// 한방향 링크드 리스트의 노드 (int형 예시)
+struct Node
+{
+	int data;
+	Node* pNext;
+};
+
+// 링크드 리스트 역할을 할 수 있는 자료형 정의
+struct LinkedList
+{
+	Node* pHead;	// 첫번째 노드
+	int currentCount;
+};
+
+void PushBack(LinkedList*, const int);
+void PushFront(LinkedList*, const int);
+int Get(LinkedList*, const int);
+void Reverse(LinkedList*);
