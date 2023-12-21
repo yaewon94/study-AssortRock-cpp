3cd7b2a7830b40ba40ecef2dbe3a619ef5e2d4e7
yaewon94 <84373511+yaewon94@users.noreply.github.com>
2023-12-21 22:50:35 +0900

    Update DynamicArray.h

--- DynamicArray.h
+++ DynamicArray.h
@@ -5,14 +5,24 @@
 //
 
 // 동적배열 역할을 수행할 수 있는 자료형 정의 (int값 예시)
-struct DynamicArray
+//struct DynamicArray
+class DynamicArray
 {
-	int* pData;	// 데이터를 저장할 주소
-	int maxLength;		// 허용 가능한 최대 길이
-	int currentLength;	// 현재 저장된 길이
+	public:
+		int* pData;	// 데이터를 저장할 주소
+		int maxLength;		// 허용 가능한 최대 길이
+		int currentLength;	// 현재 저장된 길이
+		
+		DynamicArray(const int);
+		~DynamicArray();
+		void Push(const int);
+		void Resize();
+		int Get(const int);
 };
 
+/*
 void InitArray(DynamicArray*, const int);
 void Push(DynamicArray*, const int);
 void Resize(DynamicArray*);
 int Get(DynamicArray*, const int);
+*/
