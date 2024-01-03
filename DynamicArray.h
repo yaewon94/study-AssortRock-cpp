89b065f05409e5224c41a269d36c750def6e722a
yaewon94 <84373511+yaewon94@users.noreply.github.com>
2024-01-03 14:49:37 +0900

    Update DynamicArray.h

--- DynamicArray.h
+++ DynamicArray.h
@@ -7,7 +7,7 @@ using std::cout;
 // C++ 의 vector.h 에 구현되어 있음
 //
 
-// 동적배열 역할을 수행할 수 있는 자료형 정의
+// 동적배열 역할을 수행할 수 있는 자료형 정의 (int값 예시)
 //struct DynamicArray
 template<typename T>	// 클래스 템플릿은 cpp가 아닌 h에 모두 구현해야 링킹 에러가 일어나지 않음
 class DynamicArray
@@ -16,12 +16,24 @@ class DynamicArray
 		void Resize();
 
 	public:
-		T* pData;		// 데이터를 저장할 주소
-		int maxLength;		// 허용 가능한 최대 길이
-		int currentLength;	// 현재 저장된 길이
+		T* pData;
+		int maxLength;
+		int currentLength;
 
-		DynamicArray(const int);
-		~DynamicArray();
+		// 생성자
+		DynamicArray(const int MaxLength)
+		{
+			pData = new T[MaxLength];
+			maxLength = MaxLength;
+			currentLength = 0;
+		}
+		
+		// 소멸자
+		~DynamicArray()
+		{
+			if (pData != nullptr) delete[] pData;
+		}
+
 		void Push(const T&);
 		T Get(const int);
 		
@@ -31,46 +43,70 @@ class DynamicArray
 		// => Getter, Setter 같은 함수에 적합
 		int Capacity() { return maxLength; }
 		int Size() { return currentLength; }
-		//
+
 		// [] 연산자 오버로딩
-		int& operator[](int index) { return pData[index]; }
-		//
+		T& operator[](int index) { return pData[index]; }
+
 		// 반복자 클래스
 		class iterator
 		{
 			private:
-				DynamicArray<T>* ptr;
-				int index;
+				DynamicArray<T>* dArr;
+				int currentIndex;
 
 			public:
-				iterator(DynamicArray<T>* dArr, int index) : ptr(dArr), index(index){}
+				// 생성자
+				iterator(DynamicArray<T>* dArr, int index) : dArr(dArr), currentIndex(index){}
 				
-				T& operator* () { return ptr->pData[index]; }
-				
-				void operator++ () 
-				{
-					if (ptr)
-					{
-						if (index < ptr->currentLength) ++index;
-						else assert(nullptr);
-					}
-				}
-
-				void operator-- () 
+				// 참조 연산자 오버로딩
+				T& operator* () 
 				{ 
-					if (ptr)
-					{
-						if (index > 0) --index;
-						else assert(nullptr);
-					}
+					assert(dArr 
+							&& currentIndex >= 0 
+							&& currentIndex < dArr->currentLength);
+					return dArr->pData[currentIndex];
+				}
+				
+				// 전위 연산자 오버로딩
+				iterator& operator++ () 
+				{
+					assert(dArr 
+							&& currentIndex < dArr->currentLength);
+					++currentIndex;
+					return *this;
 				}
 
+				iterator& operator-- () 
+				{ 
+					assert(dArr 
+							&& currentIndex > 0);
+					--currentIndex;
+					return *this;
+				}
+
+				// 후위 연산자 오버로딩
+				iterator operator++ (int)
+				{
+					iterator copyiter = *this;
+					++(*this);
+					return copyiter;
+				}
+
+				iterator operator-- (int)
+				{
+					iterator copyiter = *this;
+					--(*this);
+					return copyiter;
+				}
+
+				// == 연산자 오버로딩
 				bool operator== (const iterator& other)
 				{
-					if (ptr == other.ptr && index == other.index) return true;
+					if (dArr == other.dArr && currentIndex == other.currentIndex) return true;
 					else return false;
 				}
 
+				// != 연산자 오버로딩
 				bool operator!= (const iterator& other)
 				{
 					return !(*this == other);
@@ -81,15 +117,17 @@ class DynamicArray
 		iterator begin() 
 		{
 			iterator iter = iterator(this, 0);
-			cout << "iterator 지역변수 주소 테스트 = " << &iter << "\n";
+
+			// [?] iter 는 지역변수인데 함수종료뒤에 왜 안사라지는거임? 
+			// [?] 클래스 객체는 new 안써도 스택이 아니라, 힙에 생성되는건가?
+			//cout << "iterator 지역변수 주소 테스트 = " << &iter << "\n";
 			return iter; 
 		}
 
 		// DynamicArray 인덱스의 끝의 다음을 가리키는 포인터 리턴
 		iterator end()
 		{
-			iterator iter = iterator(this, this->currentLength+1);
-			return iter;
+			return iterator(this, -1);
 		}
 };
 
@@ -99,24 +137,6 @@ void Push(DynamicArray*, const int);
 void Resize(DynamicArray*);
 int Get(DynamicArray*, const int);
 */
-
-// 생성자
-template<typename T>
-DynamicArray<T>::DynamicArray(const int MaxLength)
-{
-	//pData = (int*)malloc(sizeof(int) * MaxLength);
-	pData = new T[MaxLength];
-	maxLength = MaxLength;
-	currentLength = 0;
-}
-
-// 소멸자
-template<typename T>
-DynamicArray<T>::~DynamicArray()
-{
-	//if (pData != nullptr) free(pData);
-	if (pData != nullptr) delete[] pData;
-}
 
 // 값 삽입
 template<typename T>
